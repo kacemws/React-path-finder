0a0dc2084f20ef7169d383c9fe255cada10acf02
kacemws <berras.belkacem@gmail.com>
2021-02-18 08:50:42 +0100

    base web's default setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./App";
+
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <React.StrictMode>
+        <Home />
+      </React.StrictMode>
+    </BaseProvider>
+  </StyletronProvider>,
   document.getElementById("root")
 );
 
