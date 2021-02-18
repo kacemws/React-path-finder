121d23ff55887f6a3ae00879ea274850b95bbd76
kacemws <berras.belkacem@gmail.com>
2021-02-18 21:57:40 +0100

    adjusting view to context

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,6 @@ import { Select } from "baseui/select";
 
 import "./styles.css";
 
-const App = styled("div", {
-  width: "100vw",
-  height: "100vh",
-  backgroundColor: "#121212",
-});
-const Centered = styled("div", {
-  width: "100%",
-  height: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-});
-
 const End = styled("div", {
   display: "flex",
   alignItems: "center",
@@ -30,111 +17,112 @@ const End = styled("div", {
 });
 
 export default function Home() {
-  const [value, setValue] = useState<any>([
-    { label: "Encrypt", id: "encrypt" },
-  ]);
   const [message, setMessage] = useState<number | undefined | string>("");
+  const [pattern, setPattern] = useState<number | undefined | string>("");
   return (
-    <App>
-      <Centered>
-        <div className="card-wrapper">
-          <Card
-            overrides={{
-              Root: {
-                style: {
-                  borderRadius: "8px",
-                  height: "100%",
-                  maxHeight: "100%",
-                  width: "100%",
-                  boxShadow: " 0px 1px 3px 2px #4a4b53",
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "0 1rem",
-                },
+    <div
+      className="container"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#121212",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="card-wrapper">
+        <Card
+          overrides={{
+            Root: {
+              style: {
+                borderRadius: "8px",
+                height: "100%",
+                maxHeight: "100%",
+                width: "100%",
+                boxShadow: " 0px 1px 3px 2px #4a4b53",
+                display: "flex",
+                flexDirection: "column",
+                padding: "0 1rem",
               },
-              Title: {
-                style: {
-                  width: "100%",
-                  minHeight: "4rem",
-                  maxHeight: "4rem",
-                  zIndex: 2,
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                },
+            },
+            Title: {
+              style: {
+                width: "100%",
+                minHeight: "4rem",
+                maxHeight: "4rem",
+                zIndex: 2,
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
               },
+            },
 
-              Body: {
-                style: {
-                  width: "100%",
-                  maxWidth: "100%",
-                },
+            Body: {
+              style: {
+                width: "100%",
+                maxWidth: "100%",
               },
-            }}
-            title="RSA thingy"
-            action={
-              <End>
-                <Button
-                  disabled={message == ""}
-                  overrides={{
-                    BaseButton: {
-                      style: {
-                        borderRadius: "4px",
-                        borderWidth: "2rem",
-                        borderStyle: "none",
-                        boxSizing: "border-box",
-                        padding: "0 1rem",
-                        height: "2.5rem",
-                        minWidth: "6rem",
-                        outline: "none",
-                        backgroundColor: "#197476",
-                      },
+            },
+          }}
+          title="Rabin karp demo"
+          action={
+            <End>
+              <Button
+                disabled={message == "" || pattern == ""}
+                overrides={{
+                  BaseButton: {
+                    style: {
+                      borderRadius: "4px",
+                      borderWidth: "2rem",
+                      borderStyle: "none",
+                      boxSizing: "border-box",
+                      padding: "0 1rem",
+                      height: "2.5rem",
+                      minWidth: "6rem",
+                      outline: "none",
+                      backgroundColor: "#197476",
                     },
-                  }}
-                >
-                  {value[0].id == "encrypt"
-                    ? "Start Encrypting"
-                    : "Start Dycrepting"}
-                </Button>
-              </End>
-            }
-          >
-            <StyledBody>
-              <Select
-                clearable={false}
-                options={[
-                  { label: "Encrypt", id: "encrypt" },
-                  { label: "Decrypt", id: "decrypt" },
-                ]}
-                value={value}
-                placeholder="Select operation"
-                onChange={(params) => setValue(params.value)}
-              />
-              <div
-                className="textArea-wrapper"
-                style={{
-                  marginTop: "1rem",
+                  },
                 }}
               >
-                <Textarea
-                  value={message}
-                  onChange={(e) => {
-                    setMessage((e.target as HTMLTextAreaElement).value);
-                  }}
-                  size={SIZE.large}
-                  placeholder={
-                    value[0].id == "encrypt"
-                      ? "Message to encrypt"
-                      : "Message to decrypt"
-                  }
-                  clearable
-                  clearOnEscape
-                />
-              </div>
-            </StyledBody>
-          </Card>
-        </div>
-      </Centered>
-    </App>
+                Find
+              </Button>
+            </End>
+          }
+        >
+          <StyledBody>
+            <Textarea
+              value={message}
+              onChange={(e) => {
+                setMessage((e.target as HTMLTextAreaElement).value);
+              }}
+              size={SIZE.compact}
+              placeholder={"Message"}
+              clearable
+              clearOnEscape
+            />
+            <div
+              className="textArea-wrapper"
+              style={{
+                marginTop: "1rem",
+              }}
+            >
+              <Textarea
+                value={pattern}
+                onChange={(e) => {
+                  setPattern((e.target as HTMLTextAreaElement).value);
+                }}
+                size={SIZE.compact}
+                placeholder={"Pattern"}
+                clearable
+                clearOnEscape
+              />
+            </div>
+          </StyledBody>
+        </Card>
+      </div>
+    </div>
   );
 }
